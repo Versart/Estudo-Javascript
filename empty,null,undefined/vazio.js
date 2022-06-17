9b29fcb48c928403c25463eb46959d961d0ca12e
Versart <wesleyversart@gmail.com>
2022-06-16 23:09:37 -0300

    empty vs null and undefined

---
+++ empty,null,undefined/vazio.js
@@ -0,0 +1,12 @@
+//Todos esses tipos vao retornar falso,se tratando de booleanos
+
+let pessoa
+//Undefined
+console.log(typeof pessoa)
+pessoa = ''
+//String vazia
+console.log(typeof pessoa)
+
+pessoa = null
+//Object
+console.log(typeof pessoa)
