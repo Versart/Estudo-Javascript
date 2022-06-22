858e41cd02c08ce62c311332d8d5060d273164c7
Versart <wesleyversart@gmail.com>
2022-06-21 21:58:50 -0300

    aprendendo async e await

---
+++ promises/promises.js
@@ -0,0 +1,15 @@
+async function resolvePromise() {
+  let myPromise = new Promise((resolve, reject) => {
+    window.setTimeout(() => {
+      resolve('Resolvida')
+    }, 2000)
+  })
+
+  let resolved = await myPromise
+    .then(result => result + 'passando pelo then')
+    .then(result => result + 'e agora acabou')
+    .catch(err => console.log(err.message))
+  return resolved
+}
+
+resolvePromise()
